707472c27edb88e4acca8970e1d1fc271223a4dd
Jose Garcia <josegarcia19809@gmail.com>
2025-12-12 09:20:15 -0600

    Lenguaje de plantillas Django: Pasar datos desde la vista y mostrarlos en la plantilla

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html",{
+        'titulo': 'Tienda de productos',
+        'subtitulo': 'Compra de verano'
+    })
 
 def productos(request):
     return render(request, "productos.html")
