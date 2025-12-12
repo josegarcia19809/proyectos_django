48bb90d49b5aa10a9d6d1d1181771d27de787da6
Jose Garcia <josegarcia19809@gmail.com>
2025-12-11 22:00:23 -0600

    Lenguaje de plantillas Django: Comentarios en plantillas

--- mypage/mypage/urls.py
+++ mypage/mypage/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from challenges import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("index/", views.index, name="index"),
+    path("productos/", views.productos, name="productos"),
 ]
