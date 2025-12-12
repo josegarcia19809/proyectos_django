ed8ed41bbd046975569b69be3836892745f6598b
Jose Garcia <josegarcia19809@gmail.com>
2025-12-12 13:36:53 -0600

    Lenguaje de plantillas Django: Condicionales - If templates django

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    cantidad =10
     return render(request, "index.html",{
         'titulo': 'Tienda de productos',
-        'subtitulo': 'Compra de verano'
+        'subtitulo': 'Compra de verano',
+        'cantidad': cantidad
     })
 
 def productos(request):
