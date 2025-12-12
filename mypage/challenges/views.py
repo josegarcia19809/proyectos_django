5724fcc432a4900a4fdadeaeb3f84a5504f2334a
Jose Garcia <josegarcia19809@gmail.com>
2025-12-12 13:43:16 -0600

    Lenguaje de plantillas Django: Bucles - For template django

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
-    cantidad =10
-    return render(request, "index.html",{
+    cantidad = 10
+    productos = ["TV", "Monitor", "Tablet"]
+    return render(request, "index.html", {
         'titulo': 'Tienda de productos',
         'subtitulo': 'Compra de verano',
-        'cantidad': cantidad
+        'cantidad': cantidad,
+        'productos': productos
     })
+
 
 def productos(request):
     return render(request, "productos.html")
