20c76cfac990cc38b79e6073288b682d5795c919
josegarcia19809 <josegarcia19809@gmail.com>
2026-01-29 12:49:58 -0600

    Django Meetups: Querying Data

--- django_course_site_03_models/meetups/models.py
+++ django_course_site_03_models/meetups/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 
 # Create your models here.
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    description = models.TextField()
+    city = models.CharField(max_length=100)
+    date = models.DateField()
+    attendees = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.title
--- django_course_site_03_models/meetups/views.py
+++ django_course_site_03_models/meetups/views.py
@@ -1,37 +1,22 @@
 from django.shortcuts import render
-
-meetups = [
-        {
-            "title": "Python para Ciencia de Datos",
-            "slug": "python-ciencia-datos",
-            "city": "Ciudad de México",
-            "date": "2026-02-10",
-            "attendees": 45
-        },
-        {
-            "title": "Introducción a Machine Learning",
-            "slug": "introduccion-machine-learning",
-            "city": "Guadalajara",
-            "date": "2026-02-15",
-            "attendees": 60
-        },
-        {
-            "title": "Desarrollo Web con Django",
-            "slug": "desarrollo-web-django",
-            "city": "Monterrey",
-            "date": "2026-02-20",
-            "attendees": 38
-        }
-    ]
+from .models import Meetup
 
 # Create your views here.
 def index(request):
+    meetups = Meetup.objects.all()
     return render(request, "meetups/index.html",{
         "show_meetups": True,
         "meetups": meetups
     })
 
 def meetup_details(request, meetup_slug):
-    return render(request, "meetups/meetup_details.html", {
-        "meetup_slug": meetup_slug
-    })
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(request, "meetups/meetup_details.html", {
+            "meetup_found": True,
+            "meetup_slug": selected_meetup
+        })
+    except Exception as e:
+        return render(request, "meetups/meetup_details.html", {
+            "meetup_found": False
+        })
