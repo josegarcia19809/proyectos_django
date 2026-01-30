d4730b094bd1d9cc7d5bb405b7097c034ec69738
Jose Garcia <josegarcia19809@gmail.com>
2026-01-30 10:15:30 -0600

    Django Meetups: Handling Form Submission

--- django_course_site_04_models_relations/meetups/views.py
+++ django_course_site_04_models_relations/meetups/views.py
@@ -2,23 +2,31 @@ from django.shortcuts import render
 from .models import Meetup
 from .forms import RegistrationForm
 
+
 # Create your views here.
 def index(request):
     meetups = Meetup.objects.all()
-    return render(request, "meetups/index.html",{
+    return render(request, "meetups/index.html", {
         "show_meetups": True,
         "meetups": meetups
     })
 
+
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
-        registration_form = RegistrationForm()
-        return render(request, "meetups/meetup_details.html", {
-            "meetup_found": True,
-            "meetup_slug": selected_meetup,
-            "form": registration_form
-        })
+        if request.method == "GET":
+            registration_form = RegistrationForm()
+            return render(request, "meetups/meetup_details.html", {
+                "meetup_found": True,
+                "meetup_slug": selected_meetup,
+                "form": registration_form
+            })
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetup.participants.add(participant)
     except Exception as e:
         return render(request, "meetups/meetup_details.html", {
             "meetup_found": False
