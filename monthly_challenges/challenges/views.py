4fe33ed135d32c9d58bf47a49b0a48af80bfbf7c
Jose Garcia <josegarcia19809@gmail.com>
2026-02-28 15:42:21 -0600

    django - monthly_challenges: Practicing URLs, Views & Dynamic View Logic

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -21,6 +21,17 @@ monthly_challenges = {
 from django.http import HttpResponse
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("monthly-challenge", args=[month])
+        list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
@@ -34,7 +45,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data= f"<h1>{challenge_text}</h1>"
+        response_data = f"<h1>{challenge_text}</h1>"
         return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("Mes no válido")
