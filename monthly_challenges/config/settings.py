2d1ed54c9e98a2cf332f4012c9516406451f01cc
Jose Garcia <josegarcia19809@gmail.com>
2026-03-09 17:43:11 -0600

    django - monthly_challenges: Template Inheritance

--- monthly_challenges/config/settings.py
+++ monthly_challenges/config/settings.py
@@ -55,6 +55,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             # BASE_DIR / "challenges" / 'templates',
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
