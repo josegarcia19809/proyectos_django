428ab5edc8362ae4a83b5462b638fb241b8f6a75
Jose Garcia <josegarcia19809@gmail.com>
2026-03-28 07:43:28 -0600

    Modelos y bases de datos en Django: Crear tablas basadas en modelos

---
+++ models_learning/miapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    public= models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
