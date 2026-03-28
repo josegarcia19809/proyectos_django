db5928b4ed6a3b008f064f860a6e8725740e4d37
Jose Garcia <josegarcia19809@gmail.com>
2026-03-28 14:10:11 -0600

    Modelos y bases de datos en Django: Guardar datos en la base de datos usando los modelos

--- models_learning/miapp/models.py
+++ models_learning/miapp/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
     image = models.ImageField(upload_to='images/', default='null')
-    public= models.BooleanField()
+    public = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
--- models_learning/miapp/views.py
+++ models_learning/miapp/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from miapp.models import Article
+
 
 # Create your views here.
+
+def crear_articulo(request):
+    articulo = Article(
+        title='Articulo de la vista',
+        content='Content de la vista',
+        public=True,
+
+    )
+    articulo.save()
+    return HttpResponse("Crear articulo")
