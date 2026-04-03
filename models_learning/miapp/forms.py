e8f875c0de39a41da5489b24cd3ddaf40212c012
Jose Garcia <josegarcia19809@gmail.com>
2026-04-03 10:01:36 -0600

    Formularios en Django: Validar formularios en Django

--- models_learning/miapp/forms.py
+++ models_learning/miapp/forms.py
@@ -2,20 +2,35 @@ from cProfile import label
 from random import choices
 
 from django import forms
+from django.core import validators
 
 
 class FormArticle(forms.Form):
     title = forms.CharField(
         label='Titulo',
         max_length=20,
-        required=False,
+        required=True,
         widget=forms.TextInput(
             attrs={'class': 'form-control',
-                   'placeholder': 'Ingresa el Titulo'})
+                   'placeholder': 'Ingresa el Titulo'}),
+        validators=[
+            validators.MinLengthValidator(5, "El título es demasiado corto"),
+            validators.RegexValidator(
+                regex=r'^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ., -]+$',
+                message="Solo se permiten letras y números"
+            )
+        ]
     )
     content = forms.CharField(
         label="Contenido",
-        widget=forms.Textarea()
+        widget=forms.Textarea(),
+        validators=[
+            validators.MaxLengthValidator(25, "El contenido es demasiado largo"),
+            validators.RegexValidator(
+                regex=r'^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ., -]+$',
+                message="Solo se permiten letras y números"
+            )
+        ]
     )
     content.widget.attrs.update(
         {'class': 'form-control',
