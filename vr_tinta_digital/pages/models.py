976bf0fa211117c94f37a7c6d335f6da33f0aa46
Jose Garcia <josegarcia19809@gmail.com>
2026-04-21 20:35:47 -0600

    vr Proyecto Web con Django: Modelo para la entidad de Página

--- vr_tinta_digital/pages/models.py
+++ vr_tinta_digital/pages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Título")
+    content = models.TextField(verbose_name="Contenido")
+    slug = models.CharField(max_length=255, verbose_name="Descripcion")
+    visible = models.BooleanField(verbose_name="¿Visible?")
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name="Fecha de creacion"),
+    updated_at = models.DateTimeField(auto_now=True,
+                                      verbose_name="Fecha de actualizacion")
+
+    class Meta:
+        verbose_name = "Página"
+        verbose_name_plural = "Páginas"
+
+    def __str__(self):
+        return self.title
