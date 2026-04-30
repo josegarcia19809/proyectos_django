639a1eaceb8402696f8828669938568dfa76e6f3
Jose Garcia <josegarcia19809@gmail.com>
2026-04-30 16:57:47 -0600

    vr Proyecto Web con Django: Vista y url para el listado de artículos

--- vr_tinta_digital/blog/views.py
+++ vr_tinta_digital/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from blog.models import Category
+
+
 # Create your views here.
+
+def list(request):
+    return render(request, 'articles/list.html', {
+        'title': 'Artículos',
+    })
--- vr_tinta_digital/config/urls.py
+++ vr_tinta_digital/config/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
     path('', include('pages.urls')),
+    path('', include('blog.urls')),
 ]
 
