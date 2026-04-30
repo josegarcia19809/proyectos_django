02d4b9cdd80341baeafe964e1e36a3e060ce5960
Jose Garcia <josegarcia19809@gmail.com>
2026-04-30 17:38:53 -0600

    vr Proyecto Web con Django: La potencia de las relaciones

--- vr_tinta_digital/blog/views.py
+++ vr_tinta_digital/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from blog.models import Article, Category
 
 from blog.models import Category
 
@@ -6,6 +7,8 @@ from blog.models import Category
 # Create your views here.
 
 def list(request):
+    articles = Article.objects.all()
     return render(request, 'articles/list.html', {
         'title': 'Artículos',
+        'articles': articles,
     })
