9726bef042707e51cd92e4adc86924ce183ed38b
Jose Garcia <josegarcia19809@gmail.com>
2026-05-01 10:11:21 -0600

    vr Proyecto Web con Django: Página para cada categoría

--- vr_tinta_digital/blog/views.py
+++ vr_tinta_digital/blog/views.py
@@ -13,3 +13,11 @@ def list(request):
         'title': 'Artículos',
         'articles': articles,
     })
+
+
+def category(request, category_id):
+    category = Category.objects.get(id=category_id)
+
+    return render(request, 'categories/category.html', {
+        'category': category,
+    })
