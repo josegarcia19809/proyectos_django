9251fc9740247b4b81c51ad165d3cef920a2901c
Jose Garcia <josegarcia19809@gmail.com>
2026-05-01 10:31:17 -0600

    vr Proyecto Web con Django: Mostrar error 404

--- vr_tinta_digital/blog/views.py
+++ vr_tinta_digital/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Article, Category
 
 from blog.models import Category
@@ -16,7 +16,7 @@ def list(request):
 
 
 def category(request, category_id):
-    category = Category.objects.get(id=category_id)
+    category = get_object_or_404(Category, id=category_id)
 
     return render(request, 'categories/category.html', {
         'category': category,
