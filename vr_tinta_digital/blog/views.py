732177fc85aeaf46b0eb0c65627b09a17d6a39b2
Jose Garcia <josegarcia19809@gmail.com>
2026-05-01 15:21:44 -0600

    vr Proyecto Web con Django: Cerrar sesión y restringir el acceso

--- vr_tinta_digital/blog/views.py
+++ vr_tinta_digital/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 from blog.models import Article, Category
 
 from blog.models import Category
@@ -7,6 +8,7 @@ from blog.models import Category
 
 # Create your views here.
 
+@login_required(login_url='login')
 def list(request):
     articles = Article.objects.all()
     paginator = Paginator(articles, 2)
@@ -19,7 +21,7 @@ def list(request):
         'articles': page_articles,
     })
 
-
+@login_required(login_url='login')
 def category(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     # articles = Article.objects.filter(categories=category_id)
@@ -29,7 +31,7 @@ def category(request, category_id):
         # 'articles': articles,
     })
 
-
+@login_required(login_url='login')
 def article(request, article_id):
     article = get_object_or_404(Article, id=article_id)
 
--- vr_tinta_digital/pages/views.py
+++ vr_tinta_digital/pages/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 from pages.models import Page
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
+@login_required(login_url='login')
 def page(request, slug):
     page = Page.objects.get(slug=slug)
     return render(request, "pages/page.html",{
