a91b8bc4c46c9f881cec09c606611417856fb6d4
Jose Garcia <josegarcia19809@gmail.com>
2026-05-26 18:34:22 -0600

    Uso de dataframes en django

--- django_pandas_project/django_pandas_project/urls.py
+++ django_pandas_project/django_pandas_project/urls.py
@@ -15,16 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('login/', login_view, name='login'),
+    # path('logout/', logout_view, name='logout'),
+    path('performance/', include('products.urls', namespace='products')),
+    # path('upload/', include('csvs.urls', namespace='csvs')),
+    # path('customers/', include('customers.urls', namespace='customers')),
 ]
-
 
 if settings.DEBUG:
     from django.conf.urls.static import static
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
