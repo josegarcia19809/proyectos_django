71ec577649136683f2179f1938d79ee13ee39163
Jose Garcia <josegarcia19809@gmail.com>
2026-06-19 08:42:04 -0600

    MAX. Crear un blog: Mandando datos a las vistas

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.stating_page, name='stating_page'),
-    path('posts', views.posts, name='posts-page'),
+    path('', views.starting_page, name='starting_page'),
+    path('posts', views.posts_all, name='posts-page'),
     path('posts/<slug:slug>', views.post_detail, name='post-detail_page'),
 ]
