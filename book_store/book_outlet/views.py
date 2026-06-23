a9388500935601e22eb959af13af73518171b1bc
Jose Garcia <josegarcia19809@gmail.com>
2026-06-22 21:34:57 -0600

    MAX. Book store (models): Recuperando el libro por id, agregación y ordenamiento

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Avg
+
 # from django.http import Http404
 
 from book_outlet.models import Book
@@ -6,18 +8,23 @@ from book_outlet.models import Book
 
 # Create your views here.
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("title")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg('rating'))
     return render(request, "book_outlet/index.html", {
-        "books": books})
+        "books": books,
+        "total_number_of_books": num_books,
+        "average_rating": avg_rating,
+    })
 
 
-def book_detail(request, id):
+def book_detail(request, slug):
     # try:
     #     book = Book.objects.get(id=id)
     # except Book.DoesNotExist:
     #     raise Http404
 
-    book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/book_detail.html", {
         "title": book.title,
         "author": book.author,
