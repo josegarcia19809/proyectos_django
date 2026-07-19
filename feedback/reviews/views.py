9bb6be40e6a4121b320453cc39886baa817caa37
Jose Garcia <josegarcia19809@gmail.com>
2026-07-19 17:29:11 -0600

    Max - Forms: Validación manual de formularios y los problemas con "eso".

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -6,10 +6,16 @@ from django.shortcuts import render
 def review(request):
     if request.method == 'POST':
         entered_username = request.POST['username']
+        if entered_username == '':
+            return render(request, 'reviews/review.html.html', {
+                "has_error": True
+            })
         print(entered_username)
         return HttpResponseRedirect('/thank-you')
 
-    return render(request, 'reviews/review.html')
+    return render(request, 'reviews/review.html', {
+        "has_error": False
+    })
 
 
 def thank_you(request):
