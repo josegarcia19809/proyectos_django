9fe3251255123e55fe9e9553c37ccad346da5438
Jose Garcia <josegarcia19809@gmail.com>
2026-07-19 21:02:24 -0600

    Max - Forms: Agregar más controles de formulario

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -2,7 +2,10 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    user_name = (forms.CharField(label='Your name', max_length=1,
+    user_name = (forms.CharField(label='Your name', max_length=100,
                                  error_messages={
                                      'required': 'Please enter your name',
                                      "max_length": "Please enter a shorter name", }))
+    review_text = (forms.CharField(label='Your review', widget=forms.Textarea,
+                                   max_length=1000, ))
+    rating = (forms.IntegerField(label='Your rating', min_value=1, max_value=5, ))
