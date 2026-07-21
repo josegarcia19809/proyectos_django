1054357f82c5db810c61951ff42deb5cd73d1e1f
Jose Garcia <josegarcia19809@gmail.com>
2026-07-21 08:23:15 -0600

    Max - Forms: Guardar datos con un formulario de modelo

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,11 +1,30 @@
 from django import forms
 
+from reviews.models import Review
 
-class ReviewForm(forms.Form):
-    user_name = (forms.CharField(label='Your name', max_length=100,
-                                 error_messages={
-                                     'required': 'Please enter your name',
-                                     "max_length": "Please enter a shorter name", }))
-    review_text = (forms.CharField(label='Your review', widget=forms.Textarea,
-                                   max_length=1000, ))
-    rating = (forms.IntegerField(label='Your rating', min_value=1, max_value=5, ))
+
+# class ReviewForm(forms.Form):
+#     user_name = (forms.CharField(label='Your name', max_length=100,
+#                                  error_messages={
+#                                      'required': 'Please enter your name',
+#                                      "max_length": "Please enter a shorter name", }))
+#     review_text = (forms.CharField(label='Your review', widget=forms.Textarea,
+#                                    max_length=1000, ))
+#     rating = (forms.IntegerField(label='Your rating', min_value=1, max_value=5, ))
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        labels = {
+            'user_name': 'Your name',
+            'review_text': 'Your Feedback',
+            'rating': 'Your Rating',
+        }
+        error_messages = {
+            'user_name': {
+                'required': 'Please enter your name',
+                "max_length": "Please enter a shorter name"
+            }
+        }
--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.review, name='review'),
+    # path('', views.review, name='review'),
+    path("", views.ReviewView.as_view()),
     path('thank-you', views.thank_you, name='thank_you'),
 ]
