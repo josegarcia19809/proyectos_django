bede921dfbf1c82ad486dbd1a9d94edcdb66185b
Jose Garcia <josegarcia19809@gmail.com>
2026-07-27 20:35:50 -0600

    Max - Carga de archivos: Servir archivos subidos

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,36 +1,18 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views import View
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
-from .forms import ProfileForm
 from .models import UserProfile
 
 
 # Create your views here.
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
 
-def store_file(file):
-    with open("temp/image.pdf", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
 
-
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
-
-    def post(self, request):
-        print("-" * 100)
-        print(request.FILES["image"])
-        submitted_form = ProfileForm(request.POST, request.FILES)
-
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+class ProfileListView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
