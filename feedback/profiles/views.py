16260e7e15b433fa7806c16a2e2db9d246bab9f7
Jose Garcia <josegarcia19809@gmail.com>
2026-07-27 19:52:25 -0600

    Max - Carga de archivos: Uso de modelos para el almacenamiento de archivos

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -2,14 +2,35 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
+from .forms import ProfileForm
+from .models import UserProfile
+
+
 # Create your views here.
+
+def store_file(file):
+    with open("temp/image.pdf", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
 
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        print("-"*100)
+        print("-" * 100)
         print(request.FILES["image"])
-        return  HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+            return HttpResponseRedirect("/profiles")
+
+        return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        })
